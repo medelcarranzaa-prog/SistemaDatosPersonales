38e3b6e104c02b1bd62571ca18773bd31cfd0be0
Araceli Medel Carranza <araceli@gmail.com>
2026-04-30 14:25:02 -0600

    Versión inicial del sistema

---
+++ Datos.py
@@ -0,0 +1,30 @@
+# ==========================================
+# Proyecto: Sistema de Datos Personales
+# Autor: Araceli Medel Carranza
+# Descripción:
+# Ejemplo de control de versiones con GitHub
+# ==========================================
+
+print("========================================")
+print("      SISTEMA DE DATOS PERSONALES")
+print("========================================")
+
+# Datos personales
+nombre = "Araceli"
+apellido = "Medel Carranza"
+edad = 20
+carrera = "Ingeniería en Sistemas"
+universidad = "UAGro"
+
+# Mostrar información
+print("\nInformación del estudiante:")
+print("Nombre:", nombre)
+print("Apellidos:", apellido)
+print("Edad:", edad)
+print("Carrera:", carrera)
+print("Universidad:", universidad)
+
+print("\nProyecto realizado por:")
+print(nombre, apellido)
+
+print("\nFin del programa")
